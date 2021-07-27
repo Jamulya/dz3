cda9f6545d4bc10570bb43d7af46c27c0a1876f0
Jamulya <65002206+Jamulya@users.noreply.github.com>
2021-07-27 10:31:02 -0600

    Добавила дз4

---
+++ dz_4_aijamal/main.js
@@ -0,0 +1,42 @@
+// var num = [1, 2, 4, 5, 6, 7, 8, 9, 10, 44, 50, 89]
+// 1
+// var newNum = num.map(function(e) {
+//   return e * e ;
+// })
+// console.log(newNum);
+
+// 2
+//  var result = num.filter(function(num) {
+//   return num % 2 == 0;
+// });
+// console.log(result);
+
+
+//3
+// var newNum = num.reduce((accum, item) => {
+//     accum = accum + item;
+//     return accum;
+// });
+// console.log(newNum);
+
+//4
+// var cars = ["Honda", "Mazda", "BW", "Nissan", "Lada"]
+//  var newCars = cars.map(cars => cars.charAt(0).toLowerCase() + cars.slice(0, 3));
+// console.log(newCars);
+
+//5
+// var cars = ["Honda", "Mazda", "BW", "Nissan", "Lada"]
+// var result = cars.filter(function(cars) {
+//  return cars.split('').filter(x => x.endsWith('a'));
+
+// });
+// console.log("cars", cars);
+
+//6
+// var cars = ["Honda", "Mazda", "Lexus", "Nissan", "Volkswagen"];
+//  var newcars = cars.reduce(function(sum, current){
+//    {return sum.length > current.length ? sum : current};
+//  },)
+
+//  console.log(newcars);
+  
