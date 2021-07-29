12f9237297dde6c6ef71f4ae6010ba282bee5749
Jamulya <65002206+Jamulya@users.noreply.github.com>
2021-07-29 01:18:52 -0600

    Update main.js

--- dz_4_aijamal/main.js
+++ dz_4_aijamal/main.js
@@ -14,29 +14,27 @@
 
 //3
 // var newNum = num.reduce((accum, item) => {
-//     accum = accum + item;
+//     accum = accum += item;
 //     return accum;
 // });
 // console.log(newNum);
 
 //4
 // var cars = ["Honda", "Mazda", "BW", "Nissan", "Lada"]
-//  var newCars = cars.map(cars => cars.charAt(0).toLowerCase() + cars.slice(0, 3));
+//  var newCars = cars.map(cars => cars.slice(0, 3).toLowerCase());
 // console.log(newCars);
 
 //5
 // var cars = ["Honda", "Mazda", "BW", "Nissan", "Lada"]
 // var result = cars.filter(function(cars) {
-//  return cars.split('').filter(x => x.endsWith('a'));
-
+//  return cars.endsWith('a')
 // });
-// console.log("cars", cars);
+// console.log(result);
 
 //6
 // var cars = ["Honda", "Mazda", "Lexus", "Nissan", "Volkswagen"];
-//  var newcars = cars.reduce(function(sum, current){
-//    {return sum.length > current.length ? sum : current};
+//  var longest = cars.reduce(function(longest, current){
+//    {return longest.length > current.length ? longest : current};
 //  },)
-
-//  console.log(newcars);
+//  console.log(longest);
   
